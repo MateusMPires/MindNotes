2326966dcdd922d72e7be553fc95a9f6d46ab6f4
Mateus Pires <mateusmp.df@gmail.com>
2025-07-15 09:58:44 -0300

    Initial Commit

---
+++ MindNotes/MindNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindNotesApp.swift
+//  MindNotes
+//
+//  Created by Mateus Martins Pires on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MindNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MindNotesTests/MindNotesTests.swift
@@ -0,0 +1,16 @@
+//
+//  MindNotesTests.swift
+//  MindNotesTests
+//
+//  Created by Mateus Martins Pires on 15/07/25.
+//
+
+import Testing
+
+struct MindNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
