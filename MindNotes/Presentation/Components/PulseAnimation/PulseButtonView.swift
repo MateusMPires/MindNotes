45e62b562b91d466c4c2b584ff78f114afc52211
Mateus Pires <mateusmp.df@gmail.com>
2025-08-28 23:03:25 -0300

    arrumei a view de detalhes do capítulo

---
+++ MindNotes/Presentation/Components/PulseAnimation/PulseButtonView.swift
@@ -0,0 +1,7 @@
+//
+//  FormButton.swift
+//  MindNotes
+//
+//  Created by Mateus Martins Pires on 28/08/25.
+//
+
---
+++ MindNotes/Presentation/Components/PulseAnimation/PulseCircleView.swift
@@ -0,0 +1,7 @@
+//
+//  ButtonPulseView.swift
+//  MindNotes
+//
+//  Created by Mateus Martins Pires on 28/08/25.
+//
+
