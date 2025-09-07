309bc29a83b0bfc88220a9a600b9b67d7bbf5bb7
Mateus Pires <mateusmp.df@gmail.com>
2025-09-07 19:58:15 -0300

    muitas coisas aconteceram

---
+++ MindNotes/Presentation/Views/ThoughtsContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ThoughtsContentView.swift
+//  MindNotes
+//
+//  Created by Mateus Martins Pires on 04/09/25.
+//
+
+import SwiftUI
+
+struct ThoughtsContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThoughtsContentView()
+}
